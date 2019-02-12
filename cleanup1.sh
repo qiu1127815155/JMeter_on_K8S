eb87da7eea7854802be45adedca88d9fd0b38d5c
nagarajulrpt <47053977+nagarajulrpt@users.noreply.github.com>
2019-02-12 13:04:42 +0530

    Create cleanup1.sh

---
+++ cleanup1.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+working_dir=`pwd`
+PNAME="test"
+NS=`awk '{print $NF}' "$working_dir/ns_export"`
+
+kubectl -n $NS delete services/jmeter-slave-svc-${1:-$PNAME}
+
+sleep 2
+
+kubectl -n $NS delete deployments/jmeter-slave-${1:-$PNAME}
+
+kubectl -n $NS delete deployments/jmeter-master-${1:-$PNAME}
+
+sleep 2
+
+#kubectl delete --all configmap -n $NS
+
+rm -rf $working_dir/${1:-$PNAME}
+echo "Environment has been released from cluster"
