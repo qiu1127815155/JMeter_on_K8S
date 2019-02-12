4bc5eb70fd45463b233d247e8ce02015c88c3148
nagarajulrpt <47053977+nagarajulrpt@users.noreply.github.com>
2019-02-12 13:03:35 +0530

    Update deploy_jmeter.sh

--- deploy_jmeter.sh
+++ deploy_jmeter.sh
@@ -1,49 +1,101 @@
 #!/bin/env bash
-#This script is used for launching JMeter Master and Slaves on kubernetes cluster
 
-working_dir=`pwd`
+# This script is used to launch JMeter master and slaves.
 
+WORKING_DIR=`pwd`
+
+SLAVES=1
+
+PNAME="test"
+mkdir $WORKING_DIR/${2:-$PNAME}
+echo "project name is " ${2:-$PNAME}
+
+echo
 echo "Checking kubernetes is present or not"
 
 if ! hash kubectl 2>/dev/null
 
-  then
-      echo"'kubectl' was not found in PATH"
-   exit
+then
+    echo "'kubectl' was not found in PATH"
+    echo "Kindly ensure that you can acces an existing kubernetes cluster via kubectl"
+  exit
  fi
- 
- echo
- 
+
+echo
+
  kubectl version --short
- 
+
+echo
+
+NS=ert-test
+
+ echo "Checking ERT-TEST namespace availabilty"
+
+
+ kubectl get namespace ${NS} | grep -v NAME | awk '{print $1}'
+
  echo
- 
- NS=ert-test
- 
- echo "Checking ERT-TEST namespace availablity"
- 
- kubectl get namespace $NS |grep -v NAME |awk'{print $1}'
- 
+
+ echo ${NS}" is aviable "
+
+ echo 
+
+ echo "***************Starting jmeter deploymnet****************"
+
  echo
- echo $NS " is available"
+
+# slaves=`awk '{print $NF}' "$WORKING_DIR/slaves"`
+echo "Creating your project environment configuratin files"
+
+sed "s/jmeter-slave/jmeter-slave-${2:-$PNAME}/g" $WORKING_DIR/jslave_deploy.yaml > $WORKING_DIR/${2:-$PNAME}/jslave_deploy_${2:-$PNAME}.yaml
+sed "s/jmeter-slave-svc/jmeter-slave-svc-${2:-$PNAME}/g" $WORKING_DIR/jslave_svc.yaml > $WORKING_DIR/${2:-$PNAME}/jslave_svc_${2:-$PNAME}.yaml
+sed "s/jmeter-master/jmeter-master-${2:-$PNAME}/g" $WORKING_DIR/jmaster_deploy.yaml > $WORKING_DIR/${2:-$PNAME}/jmaster_deploy_${2:-$PNAME}.yaml
+
  echo
- echo "**********Starting JMeter Deployment**********"
- echo
- echo "JMter Slave(s) deployment starting..."
- echo
- kubectl apply -n $NS -f $working_dir/jslave_deploy.yaml
- echo
- kubectl apply -n $NS -f $working_dir/jslave_svc.yaml
- echo
- echo "JMter Slave(s) deployment completed."
- echo
- echo "JMter Master deployment starting..."
- echo
- kubectl apply -n $NS -f $working_dir/jmaster_configmap.yaml
- echo
- kubectl apply -n $NS -f $working_dir/jmaster_deploy.yaml
- echo
- echo "**********JMeter Deployment Completed**********"
- echo
- kubectl get -n $NS all
-      
+
+ echo "Total JMeter slaves for this test is " ${1:-$SLAVES}
+
+echo "jmeter slave deploymnet starting......"
+
+    echo
+
+    kubectl create -n $NS -f $WORKING_DIR/${2:-$PNAME}/jslave_deploy_${2:-$PNAME}.yaml
+
+    echo
+
+    kubectl create -n $NS -f $WORKING_DIR/${2:-$PNAME}/jslave_svc_${2:-$PNAME}.yaml
+
+    echo
+
+    sleep 5
+
+    kubectl -n $NS scale deployment/jmeter-slave-${2:-$PNAME} --replicas=${1:-$SLAVES}
+
+echo "jmeter slave deployment completed."
+
+echo
+
+echo "JMeter Master deploymnet starting......"
+
+echo
+
+#kubectl create -n $NS -f $WORKING_DIR/jmaster_configmap.yaml
+
+echo
+
+kubectl create -n $NS -f $WORKING_DIR/${2:-$PNAME}/jmaster_deploy_${2:-$PNAME}.yaml
+
+
+echo
+echo "***************JMeter master deployment completed******************"
+
+echo
+kubectl get -n $NS all
+
+echo namespace = $NS > $WORKING_DIR/ns_export
+
+echo
+
+echo "Environment is ready now and you can proceed with your test"
+
+
