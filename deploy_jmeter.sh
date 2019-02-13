20d89d43d6a6071976050c4b3d0861d7d6240d7a
nagarajulrpt <47053977+nagarajulrpt@users.noreply.github.com>
2019-02-13 14:39:38 +0530

    Update deploy_jmeter.sh

--- deploy_jmeter.sh
+++ deploy_jmeter.sh
@@ -47,8 +47,8 @@ NS=ert-test
 # slaves=`awk '{print $NF}' "$WORKING_DIR/slaves"`
 echo "Creating your project environment configuratin files"
 
-sed "s/jmeter-slave/jmeter-slave-${2:-$PNAME}/g" $WORKING_DIR/jslave_deploy.yaml > $WORKING_DIR/${2:-$PNAME}/jslave_deploy_${2:-$PNAME}.yaml
-sed "s/jmeter-slave-svc/jmeter-slave-svc-${2:-$PNAME}/g" $WORKING_DIR/jslave_svc.yaml > $WORKING_DIR/${2:-$PNAME}/jslave_svc_${2:-$PNAME}.yaml
+sed "s/jmeter-slave/jmeter-slave-${2:-$PNAME}/g ; s/jmeter_mode: slave/jmeter_mode: slave-${2:-$PNAME}/g" $WORKING_DIR/jslave_deploy.yaml > $WORKING_DIR/${2:-$PNAME}/jslave_deploy_${2:-$PNAME}.yaml
+sed "s/jmeter-slave-svc/jmeter-slave-svc-${2:-$PNAME}/g ; s/jmeter_mode: slave/jmeter_mode: slave-${2:-$PNAME}/g" $WORKING_DIR/jslave_svc.yaml > $WORKING_DIR/${2:-$PNAME}/jslave_svc_${2:-$PNAME}.yaml
 sed "s/jmeter-master/jmeter-master-${2:-$PNAME}/g ; s/configname/configname-${2:-$PNAME}/g" $WORKING_DIR/jmaster_deploy.yaml > $WORKING_DIR/${2:-$PNAME}/jmaster_deploy_${2:-$PNAME}.yaml
 sed "s/configname/configname-${2:-$PNAME}/g ; s/jmeter-slave-svc/jmeter-slave-svc-${2:-$PNAME}/g" $WORKING_DIR/jmaster_config.yaml > $WORKING_DIR/${2:-$PNAME}/jmaster_config_${2:-$PNAME}.yaml
  echo
@@ -65,12 +65,14 @@ echo "jmeter slave deploymnet starting......"
 
     kubectl create -n $NS -f $WORKING_DIR/${2:-$PNAME}/jslave_svc_${2:-$PNAME}.yaml
 
+
     echo
 
-    sleep 5
+ #   sleep 5
+
 
     kubectl -n $NS scale deployment/jmeter-slave-${2:-$PNAME} --replicas=${1:-$SLAVES}
-
+echo
 echo "jmeter slave deployment completed."
 
 echo
